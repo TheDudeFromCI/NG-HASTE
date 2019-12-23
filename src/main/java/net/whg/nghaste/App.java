56e547388f6cea7c0e68a8787e219622fa939538
TheDudeFromCI <thedudefromci@gmail.com>
2019-12-22 21:15:35 -0700

    Created CI workflow.
    Signed-off-by: TheDudeFromCI <thedudefromci@gmail.com>

---
+++ src/main/java/net/whg/nghaste/App.java
@@ -0,0 +1,21 @@
+package net.whg.nghaste;
+
+/**
+ * Hello world!
+ */
+public final class App
+{
+    private App()
+    {}
+
+    /**
+     * Says hello to the world.
+     * 
+     * @param args
+     *     The arguments of the program.
+     */
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
